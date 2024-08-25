b0e52f72e00b9bc1c97a645964659b4a76b42809
mikey <mforest@protonmail.com>
2024-08-24 20:23:18 -0400

    feat: adding cursors

--- components/ProjectDisplay.vue
+++ components/ProjectDisplay.vue
@@ -46,42 +46,59 @@ onUnmounted(() => {
       <video id="video2" preload="auto">
         <source src="/Timelapse_clipped.mp4" type="video/mp4">
       </video>
+      <video id="video3" preload="auto">
+        <source src="/chr.webm" type="video/mp4">
+      </video>
+      <video id="video4" preload="auto">
+        <source src="/wedding.webp" type="video/mp4">
+      </video>
+      <video id="video5" preload="auto">
+        <source src="/yifei.webm" type="video/mp4">
+      </video>
       n
     </div>
     <nuxt-link to="/videos/cirque"
-      class="flex flex-row w-full work cursor-pointer hover:text-white transition-color duration-300"
+      class="flex flex-col md:flex-row w-full work cursor-pointer hover:text-white transition-color duration-300 pb-4 project"
       @mouseenter="handleMouseEnter('/cirque.webm')" @mouseleave="handleMouseLeave">
-      <div class="w-1/4 work-bold">Cirque Class</div>
-      <div class="w-1/4">
+      <div class="w-full md:w-1/4 work-bold pb-2">Cirque Class</div>
+      <div class="w-full md:w-1/4 pb-4">
         At the height of the pandemic lockdown, Cirque de Soleil had to solve a problem: They are based in Montreal, and
         their Emmy winning fashion designer, Zaldy Goco, is based in NYC. NativeNYVideo was able to set up a remote
         directing unit at Zaldy’s studio and lead the production on their CirqueClass.
       </div>
-      <div class="ml-auto w-1/4">
-        <img src="/cirque-thumbnail.webp" alt="Placeholder Image" class="max-w-[300px]" />
+      <div class="ml-auto w-full md:w-1/4">
+        <img src="/cirque-thumbnail.webp" alt="Placeholder Image" class="max-w-none md:max-w-[300px]" />
       </div>
     </nuxt-link>
-    <div class="flex flex-row w-full work cursor-pointer hover:text-white transition-color duration-300"
+    <div
+      class="flex flex-col md:flex-row w-full work cursor-pointer hover:text-white transition-color duration-300 pb-4 project"
       @mouseenter="handleMouseEnter('/chr.webm')" @mouseleave="handleMouseLeave">
-      <div class="w-1/4 work-bold">Church Heavenly Rest</div>
-      <div class="w-1/4">The Church of the Heavenly Rest is a famed Episcopalian masterpiece on 5th Avenue facing
+      <div class="w-full md:w-1/4 work-bold pb-2">Church Heavenly Rest</div>
+      <div class="w-full md:w-1/4 pb-4">The Church of the Heavenly Rest is a famed Episcopalian masterpiece on 5th
+        Avenue
+        facing
         Central Park. Known for hosting many film and TV shoots, CHR had to solve a problem when the time came to record
         and livestream their own series of events, starting with their annual Christmas midnight mass and continuing
         with the premiere of world renowned organist David Brigg’s “Festival of Psalms”. Out of everyone on their
         rolodex, they chose NativeNYVideo to livestream when the pressure was on. Using the same individual equipment
         that’s recorded the likes of Metallica and Lincoln Center, the results allowed the congregation who Zoom’d in to
         feel as if they were in the Church’s hallowed halls. </div>
-      <div class="ml-auto w-1/4"> <img src="/chr.webp" alt="Placeholder Image" class="max-w-[300px]" /></div>
+      <div class="ml-auto w-full md:w-1/4"> <img src="/chr.webp" alt="Placeholder Image"
+          class="max-w-none md:max-w-[300px]" /></div>
     </div>
-    <div class="flex flex-row w-full work cursor-pointer hover:text-white transition-color duration-300"
+    <div
+      class="flex flex-col md:flex-row w-full work cursor-pointer hover:text-white transition-color duration-300 pb-4 project"
       @mouseenter="handleMouseEnter('/wedding.webm')" @mouseleave="handleMouseLeave">
-      <div class="w-1/4 work-bold">Weddings and Events </div>
-      <div class="w-1/4">Did you know the wedding video industry is rampant with unprofessionalism and outright footage
+      <div class="w-full md:w-1/4 work-bold pb-2">Weddings and Events </div>
+      <div class="w-full md:w-1/4 pb-4">Did you know the wedding video industry is rampant with unprofessionalism and
+        outright footage
         theft? We know - because it's happened to us. With years of experience in our belt and a personal touch,
         NativeNYVideo is the go-to trustworthy video and photo service for wedding and event video production,
         photography, drone and photobooth services.
       </div>
-      <div class="ml-auto w-1/4"> <img src="/wedding.webp" alt="Placeholder Image" class="max-w-[300px]"></div>
+      <div class="ml-auto w-full md:w-1/4"> <img src="/wedding.webp" alt="Placeholder Image"
+          class="max-w-none md:max-w-[300px]">
+      </div>
     </div>
     <!-- <div 
       class="flex flex-row w-full work cursor-pointer hover:text-white transition-color duration-300"
@@ -101,14 +118,19 @@ onUnmounted(() => {
       <div class="w-1/4">Have a creative endeavor that needs the cinematic touch? Upgrade from looking like a grade school play shot on a camcorder to high art with NativeNYVideo. We've shot and edited for the film industry and can help you add the touch of mystique and gravitas that your project deserves.</div>
       <div class="ml-auto w-1/4"> <img src="https://via.placeholder.com/150" alt="Placeholder Image" /></div>
     </div> -->
-    <div class="flex flex-row w-full work cursor-pointer hover:text-white transition-color duration-300"
+    <div
+      class="flex flex-col md:flex-row w-full work cursor-pointer hover:text-white transition-color duration-300 pb-4 project"
       @mouseenter="handleMouseEnter('/yifei.webm')" @mouseleave="handleMouseLeave">
-      <div class="w-1/4 work-bold">Law Office of Yifei He PLLC</div>
-      <div class="w-1/4">It was a dark time to be an immigration lawyer. With imminent changes in immigration law many
+      <div class="w-full md:w-1/4 work-bold pb-2">Law Office of Yifei He PLLC</div>
+      <div class="w-full md:w-1/4 pb-4">It was a dark time to be an immigration lawyer. With imminent changes in
+        immigration
+        law many
         people needed legal counsel for their cases, but the pandemic lockdown was still in effect. Yifei He, Esq.,
         contacted NativeNYVideo in order to create a series of explainer videos to help his prospective client figure
         out what they needed, and to promote his firm. The final result was a resounding success. </div>
-      <div class="ml-auto w-1/4"> <img src="/yifei.webp" alt="Placeholder Image" class="max-w-[300px]" /></div>
+      <div class="ml-auto w-full md:w-1/4"> <img src="/yifei.webp" alt="Placeholder Image"
+          class="max-w-none md:max-w-[300px]" />
+      </div>
     </div>
     <div v-if="showVideo" :style="{ top: `${mouseY}px`, left: `${mouseX}px` }" class="fixed z-50 fade-in">
       <video width="300" height="168" autoplay muted loop>
--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,35 @@
 <template>
-  <section class="text-white text-8xl min-h-screen flex flex-col items-start justify-center -mt-10 font-bold w-full">
+  <section id="main-hero"
+    class="text-white text-8xl min-h-screen flex flex-col items-start justify-center -mt-10 font-bold w-full">
     <div class="video-wrapper">
       <video autoplay muted loop class="video-background">
         <source src="/Timelapse_clipped.mp4" type="video/mp4" />
       </video>
     </div>
     <section class="text-white h-screen flex flex-col items-start justify-end -mt-10 font-bold bottom-0 left-0 z-2">
-      <h1 class="bottom-auto md:bottom-0 top-28 md:top-auto left-0 p-10 md:p-20 max-w-[20ch] md:max-w-[18ch] fixed z-2 tracking-wide humane  text-7xl sm:text-8xl md:text-9xl">Untether Your Imagination</h1>
-      <p class="text-stone-300 text-2xl fixed right-0 bottom-0 p-10 md:p-20 max-w-[30ch] tracking-widest text-right opacity-50 work"><RotatingText/></p>
+      <h1
+        class="bottom-auto md:bottom-0 top-28 md:top-auto left-0 p-10 md:p-20 max-w-[20ch] md:max-w-[18ch] fixed z-2 tracking-wide humane  text-7xl sm:text-8xl md:text-9xl">
+        Untether Your Imagination</h1>
+      <p
+        class="text-stone-300 text-2xl fixed right-0 bottom-0 p-10 md:p-20 max-w-[30ch] tracking-widest text-right opacity-50 work">
+        <RotatingText />
+      </p>
     </section>
-    <ProjectDisplay />
+    <ProjectDisplay class="cursor-pointer" />
     <AltMenu />
 
-    <div id="chinatown" class="chinatown w-full p-10 md:p-20 overflow-hidden z-10 relative flex flex-col justify-between">
+    <div id="chinatown"
+      class="chinatown w-full p-10 md:p-20 overflow-hidden z-10 relative flex flex-col justify-between">
       <div class="absolute inset-0 bg-black bg-opacity-50"></div>
-      <div class="z-10 opacity-50 humane aboutSergio ml-auto text-center md:text-right mt-10 md:mt-8" ref="aboutSergio">About Sergio</div>
-      <span class="work text-2xl mt-10 max-w-[20ch] text-center m-auto md:ml-auto md:mr-0 md:text-right aboutText" ref="aboutText">Sergio is a native New Yorker, videographer and photographer. He is based in Queens and uses his background to inform his perspective when behind the lens. Proficient in traditional film, as well as modern techniques such as drone video capture and remote control cameras.
+      <div class="z-10 opacity-50 humane aboutSergio ml-auto text-center md:text-right mt-10 md:mt-8" ref="aboutSergio">
+        About Sergio</div>
+      <span class="work text-2xl mt-10 max-w-[20ch] text-center m-auto md:ml-auto md:mr-0 md:text-right aboutText"
+        ref="aboutText">Sergio is a native New Yorker, videographer and photographer. He is based in Queens and uses his
+        background to inform his perspective when behind the lens. Proficient in traditional film, as well as modern
+        techniques such as drone video capture and remote control cameras.
       </span>
-  
-      <GetInTouch  class="absolute bottom-0 left-1/2 transform -translate-x-1/2" />
+
+      <GetInTouch class="absolute bottom-0 left-1/2 transform -translate-x-1/2" />
       <ProfilePic class="hidden md:block md:absolute bottom-0 w-[900px] left-[25%]" />
     </div>
   </section>
@@ -110,7 +121,8 @@ onMounted(() => {
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: rgba(0, 0, 0, 0.5); /* Adjust the opacity as needed */
+  background-color: rgba(0, 0, 0, 0.5);
+  /* Adjust the opacity as needed */
 }
 
 .chinatown {
@@ -147,6 +159,7 @@ onMounted(() => {
   transform: translateY(20px);
   transition: opacity 0.5s ease-out, transform 0.5s ease-out;
 }
+
 /* Defines the animation state when image is fully visible and in position */
 .animate-slide-up {
   opacity: 1;
