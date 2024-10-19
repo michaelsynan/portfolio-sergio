a7092800943052a08dc8740c04fbceb7bd6f4ef7
mikey <mforest@protonmail.com>
2024-10-19 12:46:21 -0400

    feat: adding social links to footer

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -4,7 +4,18 @@
   <div
     class="text-white text-5xl sm:text-6xl md:text-8xl humane pt-10 mt-20 pb-6 border-2 border-stone-50 tracking-widest w-full flex justify-center bg-rose-950 text-center flex-col">
     <span>sergio tupac uzurin</span>
-    <span class="work text-xs md:text-base pt-4 opacity-30 !tracking-widest">WEBSITE BY <a href="https://formworkstudios.com" target="_blank">FORMWORKSTUDIOS</a></span>
+    <div class="flex flex-row pt-4 justify-center w-full gap-1">
+      <a href="https://www.instagram.com/nativenyvideo/" target="_blank" rel="noopener noreferrer"
+        class="cursor-pointer p-1">
+        <img src="/ig.svg" class="w-6 h-6 md:w-8 md:h-8" />
+      </a>
+      <a href="https://www.linkedin.com/in/sergiotuzurin/" target="_blank" rel="noopener noreferrer"
+        class="cursor-pointer p-1">
+        <img src="/linkedin.svg" class="w-6 h-6 md:w-8 md:h-8" />
+      </a>
+    </div>
+    <span class="work text-xs md:text-base pt-4 opacity-30 !tracking-widest">WEBSITE BY <a
+        href="https://formworkstudios.com" target="_blank">FORMWORKSTUDIOS</a></span>
   </div>
 </template>
 
