4582c70e0e91a6af0ce5dbd893777c11fc860a8b
Michael Synan <hello@michaelsynan.com>
2026-04-07 04:25:40 -0400

    updated reels

--- pages/video.vue
+++ pages/video.vue
@@ -3,10 +3,11 @@
     <h1 class="text-white text-8xl font-bold mb-12 text-center humane">Video & Film Production</h1>
     <div class="w-full max-w-4xl flex flex-col gap-10">
       <div class="flex flex-col items-center">
-        <h2 class="text-white text-3xl font-semibold mb-4">Wedding Reel</h2>
+        <h2 class="text-white text-3xl font-semibold mb-4">Creative Reel</h2>
         <div class="w-full flex justify-center aspect-w-16 aspect-h-9 border-2">
-          <video src="https://nativenyvideo.s3.amazonaws.com/wedding.webm" controls
-            poster="https://nativenyvideo.s3.amazonaws.com/wedding.webp" class="w-full h-full object-cover">
+          <video src="https://nativenyvideo.s3.us-east-1.amazonaws.com/creativereel-1080p.mp4" controls
+            poster="/creative2.webp"
+            class="w-full h-full object-cover">
             Your browser does not support the video tag.
           </video>
         </div>
@@ -14,19 +15,18 @@
       <div class="flex flex-col items-center">
         <h2 class="text-white text-3xl font-semibold mb-4">Corporate Reel</h2>
         <div class="w-full flex justify-center aspect-w-16 aspect-h-9 border-2">
-          <video src="https://nativenyvideo.s3.amazonaws.com/corporate-compressed-2.webm" controls
-            poster="https://nativenyvideo.s3.amazonaws.com/corporate-placeholder.webp"
+          <video src="https://nativenyvideo.s3.us-east-1.amazonaws.com/corporate-reel-1080p.mp4" controls
+            poster="/corporate2.webp"
             class="w-full h-full object-cover">
             Your browser does not support the video tag.
           </video>
         </div>
       </div>
       <div class="flex flex-col items-center">
-        <h2 class="text-white text-3xl font-semibold mb-4">Creative Reel</h2>
+        <h2 class="text-white text-3xl font-semibold mb-4">Wedding Reel</h2>
         <div class="w-full flex justify-center aspect-w-16 aspect-h-9 border-2">
-          <video src="https://nativenyvideo.s3.amazonaws.com/creative-compressed-2.webm" controls
-            poster="https://nativenyvideo.s3.amazonaws.com/creative-placeholder.webp"
-            class="w-full h-full object-cover">
+          <video src="https://nativenyvideo.s3.amazonaws.com/wedding.webm" controls
+            poster="https://nativenyvideo.s3.amazonaws.com/wedding.webp" class="w-full h-full object-cover">
             Your browser does not support the video tag.
           </video>
         </div>
